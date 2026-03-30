4b4afaa798e147c7d4abec83a366dacc387877d8
RafHertling <rafel_hertling@lsuz.ch>
2026-03-30 10:53:57 +0200

    completed task 1

--- cards.ts
+++ cards.ts
@@ -1,3 +1,27 @@
-export function hello(): string {
-  return "world";
+type Suit = "Rosen" | "Schellen" | "Schilten" | "Eicheln";
+
+const SUIT_RANK: Record<Suit, number> = {
+  Rosen: 3,
+  Schellen: 2,
+  Schilten: 2,
+  Eicheln: 1,
+};
+
+class Card {
+  constructor(public value: number, public suit: Suit) {}
+
+  beats(other: Card): boolean {
+    if (this.value !== other.value) {
+      return this.value > other.value;
+    }
+
+    const thisSuitRank = SUIT_RANK[this.suit];
+    const otherSuitRank = SUIT_RANK[other.suit];
+
+    if (thisSuitRank === otherSuitRank) {
+      return false;
+    }
+
+    return thisSuitRank > otherSuitRank;
+  }
 }
